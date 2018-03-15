887a0976e371c688cbee35d3f5d32d2d17c9238e
hookszhang <hookszhang@gmail.com>
2018-03-15 15:02:52 +0800

    Upgrade to Spring Boot 2.0.0.RELEASE

--- service-foo/src/main/java/space/hookszhang/service/foo/ServiceFooApplication.java
+++ service-foo/src/main/java/space/hookszhang/service/foo/ServiceFooApplication.java
@@ -3,11 +3,9 @@ package space.hookszhang.service.foo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableFeignClients
 public class ServiceFooApplication {
 
     public static void main(String[] args) {
